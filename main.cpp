25a998f5fc6e7248680b70c0a9e028c5abba8bec
Koenkk <koenkanters94@gmail.com>
2013-03-28 07:07:47 -0700

    [Linux & OS X] Check if root

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include "installer.h"
 #include <QApplication>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
+
+#if defined(Q_OS_UNIX)
+    // Check if this program is executed as root
+    QProcess lsblk;
+    lsblk.start(QString("whoami"), QIODevice::ReadOnly);
+    lsblk.waitForStarted();
+    lsblk.waitForFinished();
+    QString output = lsblk.readLine();
+    if (!output.contains("root")) {
+        qDebug () << "Not executed as root! Closing...";
+        QMessageBox::StandardButton nonRoot = QMessageBox::warning(NULL,"Insufficient privileges", "Please execute as root!",QMessageBox::Ok);
+        if (nonRoot == QMessageBox::Ok) {
+            return 0;
+        }
+    }
+#endif
+
     QApplication a(argc, argv);
     Installer w;
     w.show();
-    
     return a.exec();
 }
