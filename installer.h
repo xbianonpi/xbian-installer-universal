a5c44835ac9f1bd2615b0c682aec7cac9757c7cd
Koenkk <koenkanters94@gmail.com>
2013-03-31 13:40:39 +0200

    Removed STATE_VERIFYING

--- installer.h
+++ installer.h
@@ -55,8 +55,7 @@ private:
         STATE_GETTING_LINKS,
         STATE_GETTING_URL,
         STATE_DOWNLOADING_IMAGE,
-        STATE_WRITING_IMAGE,
-        STATE_VERIFYING
+        STATE_WRITING_IMAGE
     } state;
 
     qlonglong bytesDownloaded;
